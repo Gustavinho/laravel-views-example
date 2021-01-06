ca5d3da8cbf3cfdb1e2c40286d48a089f7e23f55
Gustavo Martinez <gustavinho.martinez@gmail.com>
2021-01-05 18:33:21 -0600

    Fixed mobile menu

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -9,8 +9,56 @@
   <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/prism.css') }}">
 </head>
-<body class="">
-  <div class="border-b border-gray-200 px-4 py-2 fixed top-0 left-0 w-full bg-white z-10">
+<body >
+  <div x-data="{ mobileMenu: false }" class="border-b border-gray-200 px-4 py-2 fixed top-0 left-0 w-full bg-white z-10">
+    <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
+    <div x-show="mobileMenu" class="lg:hidden">
+      <div class="fixed inset-0 flex z-40">
+        <!--
+          Off-canvas menu overlay, show/hide based on off-canvas menu state.
+
+          Entering: "transition-opacity ease-linear duration-300"
+            From: "opacity-0"
+            To: "opacity-100"
+          Leaving: "transition-opacity ease-linear duration-300"
+            From: "opacity-100"
+            To: "opacity-0"
+        -->
+        <div class="fixed inset-0">
+          <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
+        </div>
+        <!--
+          Off-canvas menu, show/hide based on off-canvas menu state.
+
+          Entering: "transition ease-in-out duration-300 transform"
+            From: "-translate-x-full"
+            To: "translate-x-0"
+          Leaving: "transition ease-in-out duration-300 transform"
+            From: "translate-x-0"
+            To: "-translate-x-full"
+        -->
+        <div tabindex="0" class="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none">
+          <div @click.away="mobileMenu = false" class="absolute top-0 right-0 -mr-12 pt-2">
+            <button @click="mobileMenu = false" type="button" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              <span class="sr-only">Close sidebar</span>
+              <!-- Heroicon name: x -->
+              <i data-feather="x-circle" class="text-white"></i>
+            </button>
+          </div>
+          <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
+            <div class="flex-shrink-0 flex items-center px-4 mb-8">
+              <a href="/">
+                <span class="text-900 font-bold text-xl px-2">Laravel views</span>
+              </a>
+            </div>
+            <x-menu />
+          </div>
+        </div>
+        <div class="flex-shrink-0 w-14" aria-hidden="true">
+          <!-- Force sidebar to shrink to fit close icon -->
+        </div>
+      </div>
+    </div>
     <div class="flex items-center justify-between mx-auto max-w-7xl">
       <a href="/">
         <span class="text-900 font-bold text-xl px-2">Laravel views</span>
@@ -27,9 +75,7 @@
         <button @click="mobileMenu = true" type="button" class="lg:hidden -mr-3 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900">
           <span class="sr-only">Open sidebar</span>
           <!-- Heroicon name: menu -->
-          <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-          </svg>
+          <i data-feather="menu"></i>
         </button>
       </div>
     </div>
--- resources/views/components/page.blade.php
+++ resources/views/components/page.blade.php
@@ -1,76 +1,27 @@
 <x-base>
   <!-- This example requires Tailwind CSS v2.0+ -->
-<div x-data="{ mobileMenu: false }" class="flex max-w-7xl mx-auto">
-  <!-- Off-canvas menu for mobile, show/hide based on off-canvas menu state. -->
-  <div x-show="mobileMenu" class="lg:hidden">
-    <div class="fixed inset-0 flex z-40">
-      <!--
-        Off-canvas menu overlay, show/hide based on off-canvas menu state.
-
-        Entering: "transition-opacity ease-linear duration-300"
-          From: "opacity-0"
-          To: "opacity-100"
-        Leaving: "transition-opacity ease-linear duration-300"
-          From: "opacity-100"
-          To: "opacity-0"
-      -->
-      <div class="fixed inset-0">
-        <div class="absolute inset-0 bg-gray-600 opacity-75"></div>
-      </div>
-      <!--
-        Off-canvas menu, show/hide based on off-canvas menu state.
-
-        Entering: "transition ease-in-out duration-300 transform"
-          From: "-translate-x-full"
-          To: "translate-x-0"
-        Leaving: "transition ease-in-out duration-300 transform"
-          From: "translate-x-0"
-          To: "-translate-x-full"
-      -->
-      <div tabindex="0" class="relative flex-1 flex flex-col max-w-xs w-full bg-white focus:outline-none">
-        <div @click.away="mobileMenu = false" class="absolute top-0 right-0 -mr-12 pt-2">
-          <button @click="mobileMenu = false" type="button" class="ml-1 flex items-center justify-center h-10 w-10 rounded-full focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="sr-only">Close sidebar</span>
-            <!-- Heroicon name: x -->
-            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-            </svg>
-          </button>
-        </div>
-        <div class="flex-1 h-0 pt-5 pb-4 overflow-y-auto">
-          <div class="flex-shrink-0 flex items-center px-4 mb-8">
-            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-logo-indigo-600-mark-gray-900-text.svg" alt="Workflow">
+  <div class="flex max-w-7xl mx-auto">
+    <!-- Static sidebar for desktop -->
+    <div class="fixed hidden lg:flex lg:flex-shrink-0 h-screen overflow-y-auto top-16">
+      <div class="flex flex-col w-64 sticky h-auto">
+        <!-- Sidebar component, swap this element with another sidebar if you like -->
+        <div class="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
+          <div class="mt-4 flex-grow flex flex-col relative">
+            <x-menu />
           </div>
-          <x-menu />
-        </div>
-      </div>
-      <div class="flex-shrink-0 w-14" aria-hidden="true">
-        <!-- Force sidebar to shrink to fit close icon -->
-      </div>
-    </div>
-  </div>
-
-  <!-- Static sidebar for desktop -->
-  <div class="fixed hidden lg:flex lg:flex-shrink-0 h-screen overflow-y-auto top-16">
-    <div class="flex flex-col w-64 sticky h-auto">
-      <!-- Sidebar component, swap this element with another sidebar if you like -->
-      <div class="flex flex-col flex-grow pt-5 pb-4 overflow-y-auto">
-        <div class="mt-4 flex-grow flex flex-col relative">
-          <x-menu />
         </div>
       </div>
     </div>
-  </div>
 
-  <div class="flex flex-col min-w-0 flex-1 lg:ml-64">
-    <div class="flex-1 relative z-0 flex">
-      <main class="flex-1 relative z-0 focus:outline-none xl:order-last" tabindex="0">
-        <!-- Start main area-->
-        {{ $slot }}
-        <!-- End main area -->
-      </main>
+    <div class="flex flex-col min-w-0 flex-1 lg:ml-64">
+      <div class="flex-1 relative z-0 flex">
+        <main class="flex-1 relative z-0 focus:outline-none xl:order-last" tabindex="0">
+          <!-- Start main area-->
+          {{ $slot }}
+          <!-- End main area -->
+        </main>
+      </div>
     </div>
   </div>
-</div>
 
 </x-base>
