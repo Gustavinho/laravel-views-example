f232fcc748eabecfb6ad231bab41c921b4633b12
Gustavo Martinez <gustavinho.martinez@gmail.com>
2021-02-21 20:26:15 -0600

    Compiled assets in production mode

--- resources/views/components/metrics.blade.php
+++ resources/views/components/metrics.blade.php
@@ -1,10 +1,10 @@
 <div class="px-4">
   <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-    @foreach (['pink', 'purple', 'blue'] as $tile)
+    @foreach (range(1, 3) as $tile)
       <div class="w-full shadow rounded">
-        <div class="w-full p-4 bg-white rounded border-l-4 border-{{ $tile }}-400">
+        <div class="w-full p-4 bg-white rounded border-l-4 border-pink-400">
           <div class="flex items-center">
-            <div class="p-3 bg-{{ $tile }}-100 text-{{ $tile }}-500 rounded-full mr-3">
+            <div class="p-3 bg-pink-100 text-pink-500 rounded-full mr-3">
               <i data-feather="trending-up"></i>
             </div>
             <div class="flex flex-col justify-center">
