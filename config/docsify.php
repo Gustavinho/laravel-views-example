4d1aaa952e589de5b462e3001a971a76a09dacbc
Suhendra <sayasuhendra@gmail.com>
2021-11-02 00:56:07 +0700

    added laravel-docsify to show full documentations on https://laravelviews.com/docs.

---
+++ config/docsify.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Laravel Docsify
+    |--------------------------------------------------------------------------
+    |
+    | Find your docs at the given route.
+    |
+    */
+
+    'route' => 'docs',
+    'path' => 'vendor/laravel-views/laravel-views/doc/',
+
+];
