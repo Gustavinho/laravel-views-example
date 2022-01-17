571519186d884dc797b0b7f36ded0e72b9d65970
Gustavo Martinez <gustavinho.martinez@gmail.com>
2022-01-16 21:07:20 -0600

    Updated alpine.js and tailwindcss

--- resources/views/components/base.blade.php
+++ resources/views/components/base.blade.php
@@ -7,12 +7,12 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
   @stack('meta')
   @laravelViewsStyles(livewire)
-  <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
   <link rel="stylesheet" href="{{ asset('/css/prism.css') }}">
-  <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8S2MSHNE7"></script>
 
   @production
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-B8S2MSHNE7"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,12 +12,9 @@ const mix = require('laravel-mix');
  */
 
 mix
-  .js('resources/js/app.js', 'public/js')
+  // .js('resources/js/app.js', 'public/js')
   .postCss('resources/css/app.css', 'public/css', [
-    require('postcss-import'),
     require('tailwindcss'),
-    require('postcss-nested'),
-    require('autoprefixer'),
   ]);
 
 if (mix.inProduction()) {
